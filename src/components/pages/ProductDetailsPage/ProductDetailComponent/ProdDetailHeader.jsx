524a45d87bc97f04fa7e56de710170bbdf1a74bb
fajri-farid <fajrijry14@gmail.com>
2024-07-14 10:29:52 +0800

    make img when open on prod-det full

--- src/components/pages/ProductDetailsPage/ProductDetailComponent/ProdDetailHeader.jsx
+++ src/components/pages/ProductDetailsPage/ProductDetailComponent/ProdDetailHeader.jsx
@@ -8,12 +8,12 @@ import "swiper/css/scrollbar";
 import { useState, useEffect } from "react";
 
 const formatDate = (isoString) => {
-    const date = new Date(isoString);
-    const options = { day: 'numeric', month: 'short', year: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
+  const date = new Date(isoString);
+  const options = { day: "numeric", month: "short", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
 
-    const [month, day, year] = formattedDate.replace(',', '').split(' ');
-    return `${day} ${month}, ${year}`
+  const [month, day, year] = formattedDate.replace(",", "").split(" ");
+  return `${day} ${month}, ${year}`;
 };
 
 const ProdDetailHeader = (props) => {
@@ -39,7 +39,9 @@ const ProdDetailHeader = (props) => {
     setMainContent({ src, type });
   };
 
-  const isRecommendLength = product?.Reviews?.filter((review) => review.isRecommend === true).length;
+  const isRecommendLength = product?.Reviews?.filter(
+    (review) => review.isRecommend === true
+  ).length;
 
   return (
     <>
@@ -55,13 +57,25 @@ const ProdDetailHeader = (props) => {
       <div id="gameName" className="flex justify-between">
         <h1 className="text-3xl mb-4">{product.name}</h1>
         <a href>
-          <button className="bg-buttonColorBg text-buttonColor pt-2 pr-4 pb-2 pl-4 rounded-sm">Community Hub</button>
+          <button className="bg-buttonColorBg text-buttonColor pt-2 pr-4 pb-2 pl-4 rounded-sm">
+            Community Hub
+          </button>
         </a>
       </div>
       {/* product detail side desc */}
-      <div id="headerBg" className="flex flex-row-reverse pb-4 max-w-full max-h-full">
-        <div id="sideDesc" className="ml-4 min-w-60 w-screen max-w-full max-h-full">
-          <img src={product.product_thumbnail} alt="header" className="w-full" />
+      <div
+        id="headerBg"
+        className="flex flex-row-reverse pb-4 max-w-full max-h-full"
+      >
+        <div
+          id="sideDesc"
+          className="ml-4 min-w-60 w-screen max-w-full max-h-full"
+        >
+          <img
+            src={product.product_thumbnail}
+            alt="header"
+            className="w-full"
+          />
           <p className="mobileH1 text-3xl mt-4">{product.name}</p>
           <div className="quickDesc">
             <p className="text-sm my-2">{product.short_description}</p>
@@ -86,29 +100,63 @@ const ProdDetailHeader = (props) => {
               </p>
             </div>
             <div className="reviewsScore ml-4 text-buttonColor flex flex-col">
-              <span className={`${isRecommendLength < 10 ? "text-red-500" : "text-blue-500"}`}>{isRecommendLength < 5 ? "Negative" : "Positive"}</span>
-              <span className={`${isRecommendLength < 10 ? "text-red-500" : "text-blue-500"}`}>{isRecommendLength < 5 ? "Negative" : "Positive"}</span>
-              <span className="text-fontColor leading-8">{formatDate(product.release_date)}</span>
+              <span
+                className={`${
+                  isRecommendLength < 10 ? "text-red-500" : "text-blue-500"
+                }`}
+              >
+                {isRecommendLength < 5 ? "Negative" : "Positive"}
+              </span>
+              <span
+                className={`${
+                  isRecommendLength < 10 ? "text-red-500" : "text-blue-500"
+                }`}
+              >
+                {isRecommendLength < 5 ? "Negative" : "Positive"}
+              </span>
+              <span className="text-fontColor leading-8">
+                {formatDate(product.release_date)}
+              </span>
               <span>{product.developer}</span>
               <span>{product.publisher}</span>
             </div>
           </div>
           {/* game tags */}
           <div className="genreTag mt-2 mb-2 text-xs w-full">
-            <p className="text-fontColor">Popular user-defined tags for this product:</p>
+            <p className="text-fontColor">
+              Popular user-defined tags for this product:
+            </p>
             {product?.Categories?.map((category, index) => {
-              return <span className="text-buttonColor bg-greyBg rounded-sm py-1 px-1.5 mr-0.5">{category.name}</span>;
+              return (
+                <span className="text-buttonColor bg-greyBg rounded-sm py-1 px-1.5 mr-0.5">
+                  {category.name}
+                </span>
+              );
             })}
           </div>
         </div>
         {/* game preview */}
         <div id="mainVideo" className="w-full h-full">
-          {mainContent.type === "video" ? <video controls src={mainContent.src} type="video"></video> : <img src={mainContent.src} alt="Main Preview" />}
+          {mainContent.type === "video" ? (
+            <video controls src={mainContent.src} type="video"></video>
+          ) : (
+            <img src={mainContent.src} alt="Main Preview" className="w-full"/>
+          )}
           <div className="thumbnails flex justify-between gap-1 mt-1 w-full h-full">
-            <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]} slidesPerView={5} className="lg:w-prodSliderSize lg:h-auto" scrollbar={{ draggable: true }}>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              slidesPerView={5}
+              className="lg:w-prodSliderSize lg:h-auto"
+              scrollbar={{ draggable: true }}
+            >
               {product?.ScrollThumbnails?.map((src, index) => (
                 <SwiperSlide key={index}>
-                  <img src={src.img} alt={`Screenshot ${index + 1}`} className="thumbnail" onClick={() => handleThumbnailClick(src.img, "image")} />
+                  <img
+                    src={src.img}
+                    alt={`Screenshot ${index + 1}`}
+                    className="thumbnail"
+                    onClick={() => handleThumbnailClick(src.img, "image")}
+                  />
                 </SwiperSlide>
               ))}
             </Swiper>
